220adb3ef4eed00c2d9c6d58dc939bc83e843aca
BritnyBrito <73249749+BritnyBrito@users.noreply.github.com>
2023-04-25 16:32:59 -0600

    Update 20230425143243-catalog-loan.up.sql

--- resources/migrations/20230425143243-catalog-loan.up.sql
+++ resources/migrations/20230425143243-catalog-loan.up.sql
@@ -1,1 +1,7 @@
-
+--;;
+create table catalog.prestamo (
+  prestamo_id bigint generated always as identity primary key,
+  inicio_prestamo date default current_day,
+  user_id bigint not null,
+  book_id bigint not null references catalog.book
+);

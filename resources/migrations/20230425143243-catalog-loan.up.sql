213fa1898f14a6e5a599d9f61ef4b31373f23f7b
BritnyBrito <73249749+BritnyBrito@users.noreply.github.com>
2023-04-25 18:31:32 -0600

    Delete 20230425143243-catalog-loan.up.sql

--- resources/migrations/20230425143243-catalog-loan.up.sql
+++
@@ -1,7 +0,0 @@
---;;
-create table catalog.prestamo (
-  prestamo_id bigint generated always as identity primary key,
-  inicio_prestamo date default current_day,
-  user_id bigint not null,
-  book_id bigint not null references catalog.book
-);

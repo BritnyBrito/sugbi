2dfed0ecb4edf7e8478f88b8177e858422823e04
BritnyBrito <73249749+BritnyBrito@users.noreply.github.com>
2023-04-25 19:46:17 -0600

    correcion fecha

--- resources/migrations/20230425233151-prestamos.up.sql
+++ resources/migrations/20230425233151-prestamos.up.sql
@@ -1,7 +1,7 @@
 --;;
 create table catalog.prestamo (
   prestamo_id bigint generated always as identity primary key,
-  inicio_prestamo date default current_day,
+  inicio_prestamo date not null,
   user_id bigint not null,
   book_id bigint not null references catalog.book
 );
